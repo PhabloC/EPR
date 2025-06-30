0f107f2006538a6b7d20f32344332bc595b068dd
Phablo Carvalho <phablocarvalho14@hotmail.com>
2025-06-30 14:23:08 -0300

    Scripts de monitoramento adicionados

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import "./globals.css";
 import Footer from "@/components/layout/Footer";
+import Script from "next/script";
 
 export const metadata = {
   title: "EPR Trainee 2025",
@@ -50,8 +51,61 @@ export default function RootLayout({ children }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-ZRMDXZ2VCY"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-main" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-ZRMDXZ2VCY');
+        `}</Script>
+        {/* Google Ads */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-719214096"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-ads" strategy="afterInteractive">{`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'AW-719214096');
+        `}</Script>
+        {/* Assistive Plugin */}
+        <Script id="assistive-plugin" strategy="afterInteractive">{`
+          (function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.charset="UTF-8"; s.src = 'https://cdn.assistive.com.br/plugin/AssistiveWebPlugin.js'; s.async = true; s.onload = function(){ assistive.init({});}; h.appendChild(s); })();
+        `}</Script>
+        {/* Meta Pixel */}
+        <Script id="meta-pixel" strategy="afterInteractive">{`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '153281596081732');
+          fbq('track', 'PageView');
+        `}</Script>
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KB8X7TD" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
+        {/* Meta Pixel noscript */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=153281596081732&ev=PageView&noscript=1" />`,
+          }}
+        />
         <Header />
         {children}
         <Footer />
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,81 +1,92 @@
 import Image from "next/image";
+import Script from "next/script";
 
 export default function Footer() {
   return (
-    <div
-      className="flex flex-col sm:flex-row justify-between items-center w-full h-[200px]
+    <>
+      <div
+        className="flex flex-col sm:flex-row justify-between items-center w-full h-[200px]
     sm:h-[200px]  px-6 sm:px-20 md:px-40 py-6 bg-[#003057] gap-2 sm:gap-0 "
-    >
-      <div className="flex gap-4 justify-center items-center ">
-        <Image
-          src="/logo2.png"
-          alt="logo2"
-          width={100}
-          height={100}
-          className="w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] md:w-[150px] md:h-[150px] object-contain mx-auto"
-        />
-        <Image
-          src="/cia.png"
-          alt="cia"
-          width={100}
-          height={100}
-          className="w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] md:w-[150px] md:h-[150px] object-contain mx-auto"
-        />
+      >
+        <div className="flex gap-4 justify-center items-center ">
+          <Image
+            src="/logo2.png"
+            alt="logo2"
+            width={100}
+            height={100}
+            className="w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] md:w-[150px] md:h-[150px] object-contain mx-auto"
+          />
+          <Image
+            src="/cia.png"
+            alt="cia"
+            width={100}
+            height={100}
+            className="w-[80px] h-[80px] sm:w-[120px] sm:h-[120px] md:w-[150px] md:h-[150px] object-contain mx-auto"
+          />
+        </div>
+        <div className="flex gap-4 justify-center items-center sm:pb-0 pb-8">
+          <a
+            href="https://www.linkedin.com/company/grupoepr/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/linkedin.png"
+              alt="linkedin"
+              width={32}
+              height={32}
+              className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
+            />
+          </a>
+          <a
+            href="https://www.youtube.com/@GrupoEPR"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/youtube.png"
+              alt="youtube"
+              width={32}
+              height={32}
+              className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
+            />
+          </a>
+          <a
+            href="https://www.facebook.com/grupoeprbr"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/facebook.png"
+              alt="facebook"
+              width={32}
+              height={32}
+              className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
+            />
+          </a>
+          <a
+            href="https://www.instagram.com/grupo.epr/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              src="/instagram.png"
+              alt="instagram"
+              width={32}
+              height={32}
+              className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
+            />
+          </a>
+        </div>
       </div>
-      <div className="flex gap-4 justify-center items-center sm:pb-0 pb-8">
-        <a
-          href="https://www.linkedin.com/company/grupoepr/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/linkedin.png"
-            alt="linkedin"
-            width={32}
-            height={32}
-            className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
-          />
-        </a>
-        <a
-          href="https://www.youtube.com/@GrupoEPR"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/youtube.png"
-            alt="youtube"
-            width={32}
-            height={32}
-            className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
-          />
-        </a>
-        <a
-          href="https://www.facebook.com/grupoeprbr"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/facebook.png"
-            alt="facebook"
-            width={32}
-            height={32}
-            className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
-          />
-        </a>
-        <a
-          href="https://www.instagram.com/grupo.epr/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            src="/instagram.png"
-            alt="instagram"
-            width={32}
-            height={32}
-            className="w-[32px] h-[32px] sm:w-[40px] sm:h-[40px] cursor-pointer transition duration-200 ease-in-out hover:scale-110 hover:opacity-80"
-          />
-        </a>
-      </div>
-    </div>
+      {/* Handtalk Plugin */}
+      <Script
+        src="https://plugin.handtalk.me/web/latest/handtalk.min.js"
+        strategy="afterInteractive"
+      />
+      <Script id="handtalk-init" strategy="afterInteractive">{`
+      var ht = new HT({ token: "6e3b988a1f4e4dac100d9dc587436df2", addonsEnabled: false });
+    `}</Script>
+    </>
   );
 }
